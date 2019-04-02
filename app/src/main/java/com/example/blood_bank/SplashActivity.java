1d8c4cdd579b2a8c7636b51b5da7694b219fd223
raju1998 <rajubhadwal6@gmail.com>
2019-04-02 19:17:08 +0530

    splash added

--- app/src/main/java/com/example/blood_bank/SplashActivity.java
+++ app/src/main/java/com/example/blood_bank/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.blood_bank;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2000);
     }
 }
